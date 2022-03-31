ec042af656600e126715304604072e5646e7ace2
小布 <www.1714005186@gmail.com>
2022-03-31 18:58:12 +0800

    移除文件夹,青龙面板不支持文件夹层级

---
+++ Dep-PromiseUtil.js
@@ -0,0 +1,45 @@
+const $ = require("./Dep-Env");
+
+module.exports = class PromiseUtil {
+	static GET = "get";
+	static POST = "post";
+
+	/**
+	 * 构造并返回 Promise
+	 *
+	 * @param baseUrl 基地址
+	 * @param url 接口名
+	 * @param method 请求方式
+	 * @param headers 请求头
+	 * @param body 请求体
+	 * @param resHandelr 结果处理器
+	 * */
+	static buildPromise(baseUrl, url, method, headers, body, resHandler) {
+		if (!$[method]) {
+			throw new Error("未知方法");
+		}
+
+		let option = {
+			url: `${baseUrl}${url}`,
+			headers: headers,
+			timeout: 10000,
+		};
+
+		if (body) {
+			option.body = body;
+		}
+
+		return new Promise((resolve, reject) => {
+			$[method](option, (err, resp, data) => {
+				if (err) {
+					reject(err);
+				} else {
+					if (resHandler) {
+						data = resHandler(data);
+					}
+					resolve(data);
+				}
+			});
+		});
+	}
+};
---
+++ Task-Nuggets.js
@@ -0,0 +1,54 @@
+/*
+[Script]
+cron "0 8 * * *" tag=掘金签到-抽奖-获取矿石数量
+*/
+const $ = require("./Dep-Env");
+const NotifyUtil = require("./sendNotify");
+const NuggetsClient = require("./Dep-NuggetsClient");
+
+$.cookie = process.env.JUEJIN_COOKIE;
+
+/**
+ * 任务执行流程
+ */
+async function processFlow() {
+	// 1. 检查 Cookie 是否存在
+	if (!$.cookie) {
+		console.log("未获取到掘金Cookie!");
+		NotifyUtil.sendNotify($.name, `未获取到掘金Cookie!`);
+		return;
+	}
+
+	var nuggetsClient = new NuggetsClient($.cookie);
+
+	// 2. 检查 Cookie 是否有效
+	let valid = await nuggetsClient.validateUser();
+	if (!valid) {
+		console.log("用户Cookie已失效!");
+		NotifyUtil.sendNotify($.name, `用户Cookie已失效!`);
+		return;
+	}
+
+	// 3. 开始签到
+	let signInfo = await nuggetsClient.signIn();
+	console.log("签到信息:" + signInfo);
+
+	// 4. 开始抽奖
+	let lotteryInfo = await nuggetsClient.lottery();
+	console.log("抽奖结果:" + lotteryInfo);
+
+	// 5. 获取剩余矿石信息
+	let oreInfo = await nuggetsClient.getAmountOfOre();
+	console.log("剩余矿石信息:" + oreInfo);
+
+	NotifyUtil.sendNotify(
+		$.name,
+		`
+    \n签到结果[${signInfo}]
+    \n抽奖结果[${lotteryInfo}]
+    \n获取矿石数量[${oreInfo}]
+    `
+	);
+}
+
+processFlow();
